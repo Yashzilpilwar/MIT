76bc03a8129b852557c8105d1e92350149b71cec
Demo <jayzilpilwar@gmail.com>
2024-05-07 22:46:39 +0530

    DONE

---
+++ src/test/java/com/example/mit/MitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
